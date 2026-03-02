3d54bee061caba86aee6ef6cadce65b9dc6879a2
juanpablot1707-dotcom <juanpablot1707@gmail.com>
2026-03-02 14:41:42 -0500

    Actualización frontend React rama movil

--- frontend-react/frontend-react/src/App.jsx
+++ frontend-react/frontend-react/src/App.jsx
@@ -1,40 +1,10 @@
-import "./App.css";
-import fondo from "./assets/fondo.jpeg";
+import RecyclingRegister from "./Registro"
 
-
-
-function App() {
-  return (
-    <div
-      className="container"
-      style={{ backgroundImage: `url(${fondo})` }}
-    >
-      <div className="login-box">
-        <h2>Bienvenido a recycling points</h2>
-
-        <div className="logo">
-          <div className="circle">
-            🌿
-          </div>
-        </div>
-
-        <input type="email" placeholder="Correo" />
-        <input type="password" placeholder="Contraseña" />
-
-        <div className="buttons">
-          <button className="btn-primary">INICIO DE SESIÓN</button>
-          <button className="btn-secondary">CREAR CUENTA</button>
-        </div>
-
-        <a href="#" className="forgot">
-          ¿Olvidaste tu contraseña?
-        </a>
-
-        <button className="facebook">FACEBOOK</button>
-        <button className="google">GOOGLE</button>
-      </div>
+function App (){
+  return(
+    <div>
+      <RecyclingRegister></RecyclingRegister>
     </div>
-  );
+  )
 }
-
 export default App;
