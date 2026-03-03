17c6cad380cd5c152eb4e2b978971520d2be8037
Angie Escobar <escobarav103@gmail.com>
2026-03-02 20:46:57 -0500

    feat: pantalla de inicio web con React Router

--- frontend-react/frontend-react/src/App.jsx
+++ frontend-react/frontend-react/src/App.jsx
@@ -1,10 +1,16 @@
-import RecyclingRegister from "./Registro"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import RecyclingRegister from "./Registro";
 
-function App (){
-  return(
-    <div>
-      <RecyclingRegister></RecyclingRegister>
-    </div>
-  )
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registro" element={<RecyclingRegister />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
